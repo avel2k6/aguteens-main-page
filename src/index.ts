ae7d1071b601a2a8058d79cf887ddbadd3882d9e
BykovVV <bykovvv@consultant.ru>
2025-11-01 16:38:21 +0300

    - Кнопка покупки - Бекграунд для футера

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import './components/block-reviews';
 import './components/block-partners';
 import './components/block-video';
 import './components/footer';
+import './components/buy-button';
 
 import { initList, TList } from './components/list';
 import { slowInfiniteScroll } from './components/horizontal-scroll';
