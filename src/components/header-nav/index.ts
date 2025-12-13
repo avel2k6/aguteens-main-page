eb4d6f9a5e654badb86c920b9986e613b3a5ec65
BykovVV <bykovvv@consultant.ru>
2025-12-13 16:18:08 +0300

    - Мобильное меню

--- src/components/header-nav/index.ts
+++ src/components/header-nav/index.ts
@@ -1,5 +1,65 @@
 import './index.less';
 
-export const initHeaderNav = () => {
+const attributes = {
+    showNav: 'data-show-nav',
+    closeNav: 'data-close-nav',
+};
 
+const classes = {
+    component: 'header-nav',
+    visible: 'header-nav_visible',
+};
+
+
+const bodyClasses = {
+    // Класс убирает прокрутку, когда открыто мобильное меню
+    noOverflow: 'nav-body-no-mobile-overflow',
+};
+
+const headerClasses = {
+    // Класс убирает анимации, когда открыто меню, чтобы работал блюр
+    noAnimation: 'nav-header-no-animation',
+};
+
+export const initHeaderNav = () => {
+    const state = {
+        isOpened: false,
+    };
+
+    const component = document.querySelector(`.${classes.component}`);
+    if (!component) {
+        return;
+    }
+
+    const opener = document.querySelector(`[${attributes.showNav}]`);
+    const closer = document.querySelector(`[${attributes.closeNav}]`);
+    const header = document.querySelector('.header');
+
+    const handleShow = () => {
+        state.isOpened = true;
+        render();
+
+    };
+
+    const handleClose = () => {
+        state.isOpened = false;
+        render();
+    };
+
+    opener?.addEventListener('click', handleShow);
+    closer?.addEventListener('click', handleClose);
+
+
+    const render = () => {
+        if (state.isOpened) {
+            component.classList.add(classes.visible);
+            document.body.classList.add(bodyClasses.noOverflow);
+            header.classList.add(headerClasses.noAnimation);
+            return;
+        }
+
+        component.classList.remove(classes.visible);
+        document.body.classList.remove(bodyClasses.noOverflow);
+        header.classList.remove(headerClasses.noAnimation);
+    };
 };
